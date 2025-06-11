46e8eb94e2eab372654a5572a7c8c89a61539e1d
OLADIPO OLABIYI <ddolabiy@uwaterloo.ca>
2025-06-11 15:11:59 -0400

    updated firebase.ts

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCeR3TMp-4xjrDrKQYkqagL_niBm6bRUYs",
-  authDomain: "marcan-c00b3.firebaseapp.com",
-  projectId: "marcan-c00b3",
-  storageBucket: "marcan-c00b3.firebasestorage.app",
-  messagingSenderId: "1056331122103",
-  appId: "1:1056331122103:web:ef7a1a20f2bafd21e59432",
-  measurementId: "G-1SE78YJXN3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
